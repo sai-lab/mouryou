c2bb68a5b19588a6ca23eb29b3b82bd13bd11d38
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-02-23 16:48:02 +0900

    Ctrl-Cの処理を追加

--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"./lib/apache"
 	"./lib/tenbin"
-	"fmt"
+	"log"
+	"os"
+	"os/signal"
 	"strconv"
+	"time"
 )
 
 func main() {
 	var hypervisor tenbin.Hypervisor
 	apache.Timeout = 1
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+
+	go func() {
+		for range sig {
+			os.Exit(0)
+		}
+	}()
 
 	for i := 1; i < 10; i++ {
 		num := strconv.Itoa(i)
 		hypervisor.AddVM("web-server-"+num, "192.168.11.2"+num)
 	}
 
-	fmt.Printf("%+v\n", hypervisor)
-	fmt.Printf("%+v\n", hypervisor.AVGOR())
+	for {
+		log.Printf("%+v\n", hypervisor.AVGOR())
+		time.Sleep(time.Second)
+	}
 }
