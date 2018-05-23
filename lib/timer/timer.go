9014db0f9abf62794b99f3f1d31efdda73806983
mm-matsuda <joniyjoniylo@gmail.com>
2018-05-23 17:25:45 +0900

    起動処理後の停止処理待ち時間の処理を修正

--- lib/timer/timer.go
+++ lib/timer/timer.go
@@ -8,11 +8,7 @@ import (
 )
 
 func Set(flag *int, flagMutex *sync.RWMutex, wait time.Duration) {
-	if mutex.Read(flag, flagMutex) > 0 {
-		return
-	}
-
-	mutex.Write(flag, flagMutex, 1)
+	mutex.Write(flag, flagMutex, *flag+1)
 	time.Sleep(wait * time.Second)
-	mutex.Write(flag, flagMutex, 0)
+	mutex.Write(flag, flagMutex, *flag-1)
 }
