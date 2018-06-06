c0cbd8d58a04b40082e868913fab817c5c8031b8
joniyjoniy <s17g480@stu.kagawa-u.ac.jp>
2018-06-06 19:24:03 +0900

    load_monitorの変数を整理

--- lib/monitor/load_monitoring.go
+++ lib/monitor/load_monitoring.go
@@ -41,33 +41,48 @@ func LoadMonitoring(config *models.Config) {
 }
 
 func Ratios(states []apache.ServerStatus) ([]float64, [11][]string) {
-	var group sync.WaitGroup
-	var mutex sync.Mutex
-	var ds []Data
+	var (
+		operatingRatio    = 0
+		cpuUsedPercent    = 1
+		throughput        = 2 // throughputs is ApacheLog in apache.ServerStatus
+		dstatLog          = 3
+		memoryUsedPersent = 4
+		memoryBuffer      = 5
+		memoryCached      = 6
+		memoryStat        = 7
+		acquisitionTime   = 8
+		critical          = 9
+		reqPerSec         = 10 // reqPerSecは起動してからの平均の1秒間のリクエスト数
+		group             sync.WaitGroup
+		mutex             sync.Mutex
+		ds                []Data       // dataはオートスケールに用いる
+		arrs              [11][]string // arrsはログ記録や重み調整に用いる
+	)
 
 	length := len(states)
 	ors := make([]float64, length)
-	var arrs [11][]string
 
 	for i := 0; i < 11; i++ {
 		arrs[i] = make([]string, length+1)
 	}
 
-	arrs[0][0] = "ors"
-	arrs[1][0] = "crs"
-	arrs[2][0] = "tps"
-	arrs[3][0] = "dls"
-	arrs[4][0] = "mps"
-	arrs[5][0] = "buffers"
-	arrs[6][0] = "caches"
-	arrs[7][0] = "memalls"
-	arrs[8][0] = "times"
-	arrs[9][0] = "critical"
-	arrs[10][0] = "rpss"
+	// 各配列の先頭に何の配列か記載
+	arrs[operatingRatio][0] = "ors"
+	arrs[cpuUsedPercent][0] = "crs"
+	arrs[throughput][0] = "tps"
+	arrs[dstatLog][0] = "dls"
+	arrs[memoryUsedPersent][0] = "mps"
+	arrs[memoryBuffer][0] = "buffers"
+	arrs[memoryCached][0] = "caches"
+	arrs[memoryStat][0] = "memalls"
+	arrs[acquisitionTime][0] = "times"
+	arrs[critical][0] = "critical"
+	arrs[reqPerSec][0] = "rpss"
 
 	for i, v := range states {
 		group.Add(1)
 		var data Data
+		// 各サーバの付加情報毎に実行
 		go func(i int, v apache.ServerStatus) {
 			defer group.Done()
 			mutex.Lock()
@@ -76,14 +91,16 @@ func Ratios(states []apache.ServerStatus) ([]float64, [11][]string) {
 			data.Name = v.HostName
 			id := "[" + strconv.FormatInt(int64(v.Id), 10) + "]"
 
-			if v.Other != "" {
+			// Otherはタイムアウトした場合"Connection is timeout."が入る。
+			if v.Other == "Connection is timeout." {
+				// タイムアウトした場合、稼働率を1にほかを0にする。
 				ors[i] = 1
-				arrs[0][i+1] = id + "1"
+				arrs[operatingRatio][i+1] = id + "1"
 				for k := 1; k < 9; k++ {
 					arrs[k][i+1] = id + "0"
 				}
-				arrs[9][i+1] = id + v.Other
-				arrs[10][i+1] = id + "0"
+				arrs[critical][i+1] = id + v.Other
+				arrs[reqPerSec][i+1] = id + "0"
 				data.Operating = 1
 				data.CPU = 0
 				data.Throughput = 0
@@ -93,6 +110,7 @@ func Ratios(states []apache.ServerStatus) ([]float64, [11][]string) {
 				} else {
 					data.Throughput = (v.ApacheLog - beforeTotalAccess[v.HostName]) / (int(time.Now().Unix()) - beforeTime[v.HostName])
 				}
+
 				beforeTime[v.HostName] = int(time.Now().Unix())
 				beforeTotalAccess[v.HostName] = v.ApacheLog
 				beforeTime[v.HostName] = int(time.Now().Unix())
@@ -101,20 +119,20 @@ func Ratios(states []apache.ServerStatus) ([]float64, [11][]string) {
 				data.Operating = ors[i]
 				data.CPU = v.CpuUsedPercent[0]
 
-				ds = append(ds, data)
-				arrs[0][i+1] = id + fmt.Sprintf("%.5f", ors[i])
-				arrs[1][i+1] = id + fmt.Sprintf("%3.5f", v.CpuUsedPercent[0])
-				arrs[2][i+1] = id + fmt.Sprintf("%5d", v.ApacheLog)
-				arrs[3][i+1] = id + v.DstatLog
-				arrs[4][i+1] = id + fmt.Sprintf("%3.5f", v.MemStat.UsedPercent)
-				arrs[5][i+1] = id + fmt.Sprintf("%3.5d", v.MemStat.Buffers)
-				arrs[6][i+1] = id + fmt.Sprintf("%3.5d", v.MemStat.Cached)
-				arrs[7][i+1] = id + fmt.Sprint(v.MemStat)
-				arrs[8][i+1] = id + v.Time
-				arrs[10][i+1] = id + fmt.Sprintf("%6.2f", v.ReqPerSec)
+				arrs[operatingRatio][i+1] = id + fmt.Sprintf("%.5f", ors[i])
+				arrs[cpuUsedPercent][i+1] = id + fmt.Sprintf("%3.5f", v.CpuUsedPercent[0])
+				arrs[throughput][i+1] = id + fmt.Sprintf("%5d", v.ApacheLog)
+				arrs[dstatLog][i+1] = id + v.DstatLog
+				arrs[memoryUsedPersent][i+1] = id + fmt.Sprintf("%3.5f", v.MemStat.UsedPercent)
+				arrs[memoryBuffer][i+1] = id + fmt.Sprintf("%3.5d", v.MemStat.Buffers)
+				arrs[memoryCached][i+1] = id + fmt.Sprintf("%3.5d", v.MemStat.Cached)
+				arrs[memoryStat][i+1] = id + fmt.Sprint(v.MemStat)
+				arrs[acquisitionTime][i+1] = id + v.Time
+				arrs[reqPerSec][i+1] = id + fmt.Sprintf("%6.2f", v.ReqPerSec)
 				if ors[i] == 1 && v.CpuUsedPercent[0] >= 100 {
-					arrs[9][i+1] = "[" + id + "]" + "Operating Ratio and CPU UsedPercent is MAX!"
+					arrs[critical][i+1] = id + "Operating Ratio and CPU UsedPercent is MAX!"
 				}
+				ds = append(ds, data)
 			}
 		}(i, v)
 	}
