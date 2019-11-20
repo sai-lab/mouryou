bd1eee895910edfbefad365d4c5b64aa36f8bd75
shiro8945 <s16t259@stu.kagawa-u.ac.jp>
2019-11-20 12:43:32 +0900

    renewal

--- lib/engine/destination_setting.go
+++ lib/engine/destination_setting.go
@@ -7,7 +7,6 @@ import (
 	"github.com/sai-lab/mouryou/lib/databases"
 	"github.com/sai-lab/mouryou/lib/logger"
 	"github.com/sai-lab/mouryou/lib/models"
-	"github.com/sai-lab/mouryou/lib/monitor"
 	"github.com/sai-lab/mouryou/lib/mutex"
 	"github.com/sai-lab/mouryou/lib/timer"
 	"golang.org/x/net/websocket"
@@ -15,7 +14,7 @@ import (
 
 // DestinationSetting では，monitor.PowerChを常に受信し，
 // 受信したチャネルに従って，振分先情報の追加・削除をロードバランサに通知します．
-func DestinationSetting(config *models.Config) {
+/*func DestinationSetting(config *models.Config) {
 	var b, s, w, o int
 	var connection *websocket.Conn
 	var err error
@@ -107,4 +106,98 @@ func DestinationSetting(config *models.Config) {
 			}
 		}
 	}
+}
+*/
+
+func DestinationSetting(config *models.Config, power PowerStruct) {
+	var b, s, w, o int
+	var connection *websocket.Conn
+	var err error
+
+	if config.UseWeb {
+		connection, err = config.WebSocket.Dial()
+	}
+
+	// 稼働中の台数
+	w = mutex.Read(&working, &workMutex)
+	// 起動処理中の台数
+	b = mutex.Read(&booting, &bootMutex)
+	// 停止処理中の台数
+	s = mutex.Read(&shutting, &shutMutex)
+
+	// データベースとログに稼働状況を記録
+	tags := []string{
+		"parameter:working_log",
+		"operation:power",
+		fmt.Sprintf("host:%s", power.Name),
+		fmt.Sprintf("load:%s", power.Load),
+	}
+	fields := []string{
+		fmt.Sprintf("working:%d", w),
+		fmt.Sprintf("booting:%d", b),
+		fmt.Sprintf("shutting:%d", s),
+		fmt.Sprintf("power:%s", power.Info),
+	}
+	logger.Record(tags, fields)
+	databases.WriteValues(config.InfluxDBConnection, config, tags, fields)
+
+	if config.DevelopLogLevel >= 1 {
+		fmt.Println("PowerCh comming ", power.Name, power.Info)
+	}
+
+	switch power.Info {
+	case "booting up": // 起動処理を開始した
+		// 起動処理中の台数を増加
+		mutex.Write(&booting, &bootMutex, b+1)
+		if config.UseWeb {
+			logger.Send(connection, err, "Booting up: "+power.Name)
+		}
+	case "booted up": // 起動処理が完了した
+		// ロードバランサの振分先にpower.Nameを追加
+		err := config.Cluster.LoadBalancer.Active(config.Cluster.VirtualMachines[power.Name].Name)
+		if err != nil {
+			place := logger.Place()
+			logger.Error(place, err)
+		}
+		if config.UseWeb {
+			logger.Send(connection, err, "Booted up: "+power.Name)
+		}
+		// 稼働中の台数を増加
+		mutex.Write(&working, &workMutex, w+1)
+		// 起動処理が終わったので，起動処理中の台数を減少
+		mutex.Write(&booting, &bootMutex, b-1)
+		// 起動処理が完了した後，config.Wait秒間は停止処理を発火しないようにwaitingを設定
+		go timer.Set(&waiting, &waitMutex, config.Wait)
+	case "shutting down": // 停止処理を開始した
+		// 停止処理中の台数を増加
+		mutex.Write(&shutting, &shutMutex, s+1)
+		// 稼働中の台数を減少
+		mutex.Write(&working, &workMutex, w-1)
+		// ロードバランサの振分先からpower.Nameを削除
+		err := config.Cluster.LoadBalancer.Inactive(config.Cluster.VirtualMachines[power.Name].Name)
+		if err != nil {
+			place := logger.Place()
+			logger.Error(place, err)
+		}
+		// 停止処理を開始した後，config.Wait秒間は停止処理を発行しないようにwaitingを設定
+		go timer.Set(&waiting, &waitMutex, config.Wait)
+		if config.UseWeb {
+			logger.Send(connection, err, "Shutting down: "+power.Name)
+		}
+	case "shutted down": // 停止処理が完了した
+		// 停止処理が終わったので，停止処理中の台数を減少
+		mutex.Write(&shutting, &shutMutex, s-1)
+		if config.UseWeb {
+			logger.Send(connection, err, "Shutted down: "+power.Name)
+		}
+	default:
+		fmt.Println("Error:", power)
+		switch {
+		case strings.Index(power.Info, "domain is already running") != -1:
+			mutex.Write(&booting, &bootMutex, o-1)
+		case strings.Index(power.Info, "domain is not running") != -1:
+			mutex.Write(&shutting, &shutMutex, s-1)
+		}
+	}
+
 }
--- lib/engine/server_management.go
+++ lib/engine/server_management.go
@@ -13,7 +13,7 @@ import (
 // ServerManagement は稼働状況と負荷状況に基いてオートスケールを実行します.
 // 稼働状況はengine/destination_setting.goが設定しています.
 // 負荷状況はengine/load_determination.goが判断します.
-func ServerManagement(config *models.Config) {
+/*func ServerManagement(config *models.Config) {
 	var b, s, w, wait int
 	var order autoScaleOrder
 
@@ -56,10 +56,56 @@ func ServerManagement(config *models.Config) {
 		}
 	}
 }
+*/
+
+func ServerManagement(config *models.Config) {
+	var b, s, w, wait int
+	var order autoScaleOrder
+
+	vmNum := len(config.Cluster.VirtualMachines)
+	arm := len(config.AlwaysRunningMachines)
+
+	for order = range autoScaleOrderCh {
+		w = mutex.Read(&working, &workMutex)
+		b = mutex.Read(&booting, &bootMutex)
+		s = mutex.Read(&shutting, &shutMutex)
+		wait = mutex.Read(&waiting, &waitMutex)
+
+		tags := []string{"parameter:working_log", "operation:server_management"}
+		fields := []string{fmt.Sprintf("working:%d", w),
+			fmt.Sprintf("booting:%d", b),
+			fmt.Sprintf("shutting:%d", s),
+			fmt.Sprintf("waiting:%s", wait),
+			fmt.Sprintf("load:%s", order.Load),
+			fmt.Sprintf("handle:%s", order.Handle),
+			fmt.Sprintf("weight:%d", order.Weight),
+		}
+		logger.Record(tags, fields)
+		databases.WriteValues(config.InfluxDBConnection, config, tags, fields)
+
+		if config.DevelopLogLevel >= 3 {
+			place := logger.Place()
+			logger.Debug(place, fmt.Sprintf("Load: %s, Handle: %s, Weight: %d", order.Load, order.Handle, order.Weight))
+		}
+		switch order.Handle {
+		case "ScaleOut":
+			if w+b+wait < vmNum && s == 0 {
+				bootUpVMs(config, order.Weight, order.Load)
+			}
+		case "ScaleIn":
+			if w > arm && b == 0 {
+				shutDownVMs(config, order.Weight, order.Load)
+			}
+		default:
+			place := logger.Place()
+			logger.Debug(place, "Unknown Handle is comming!")
+		}
+	}
+}
 
 // bootUpVMs は引数に 設定値用構造体 config, 起動したいサーバの重み weight, 判断基準にした負荷量 load をとります．
 // 複数形だけど一台ずつ起動処理をbootUpVMに投げます．
-func bootUpVMs(config *models.Config, weight int, load string) {
+/*func bootUpVMs(config *models.Config, weight int, load string) {
 	// 充分な重みを持つ単体のサーバが存在しない場合に，起動する候補となる重みの小さいサーバの添字を格納する配列
 	var candidate []int
 	serverStates := monitor.GetServerStates()
@@ -82,6 +128,63 @@ func bootUpVMs(config *models.Config, weight int, load string) {
 
 	if len(candidate) == 0 {
 		// 起動候補が存在しない場合何もせずreturn
+		return
+	}
+
+	// 起動候補サーバの中から最も重みの大きいサーバを起動
+	toBootUp := candidate[0]
+	for _, n := range candidate {
+		if serverStates[n].Weight > serverStates[toBootUp].Weight {
+			toBootUp = n
+		}
+	}
+	go bootUpVM(config, serverStates[toBootUp], load)
+	mutex.Write(&futureTotalWeight, &futureTotalWeightMutex, futureTotalWeight+serverStates[toBootUp].Weight)
+}
+*/
+
+func bootUpVMs(config *models.Config, weight int, load string) {
+	// 充分な重みを持つ単体のサーバが存在しない場合に，起動する候補となる重みの小さいサーバの添字を格納する配列
+	var candidate []int
+	var cand []int
+	serverStates := monitor.GetServerStates()
+
+	for i, serverState := range serverStates {
+		if serverState.Info != "shutted down" {
+			// 停止中のサーバ以外は無視
+			continue
+		}
+
+		if serverState.Info == "waiting" {
+			// 待ち状態のサーバを復帰
+			go bootWaiting(config, serverState, load)
+			mutex.Write(&futureTotalWeight, &futureTotalWeightMutex, futureTotalWeight+serverState.Weight)
+			return
+		}
+
+		if serverState.Weight >= weight {
+			cand = append(cand)
+			continue
+		}
+		// サーバの重さが必要な重み未満の場合candidateに追加
+		candidate = append(candidate, i)
+	}
+
+	if len(candidate) == 0 && len(cand) == 0 {
+		// 起動候補が存在しない場合何もせずreturn
+		return
+	}
+
+	if len(cand) > 0 {
+		toBootUp := candidate[0]
+		for _, n := range candidate {
+			if serverStates[n].Weight > serverStates[toBootUp].Weight {
+				toBootUp = n
+			}
+		}
+		// サーバの重さが必要な重み以上なら起動処理を任せてreturn
+		go bootUpVM(config, serverState[toBootUp], load)
+		mutex.Write(&futureTotalWeight, &futureTotalWeightMutex, futureTotalWeight+serverStates[toBootUp].Weight)
 		return
 	}
 
@@ -132,8 +235,43 @@ func bootUpVM(config *models.Config, serverState monitor.ServerState, load strin
 	mutex.Write(&totalWeight, &totalWeightMutex, totalWeight+serverState.Weight)
 }
 
+func bootWaiting(config *models.Config, serverState monitor.ServerState, load string) {
+	var power monitor.PowerStruct
+
+	// これから起動処理を発行することを通知
+	power.Name = serverState.Name
+	power.Info = "booting up"
+	power.Load = load
+	serverState.Info = "booting up"
+	if monitor.PowerCh != nil {
+		monitor.PowerCh <- power
+	}
+	if monitor.StateCh != nil {
+		monitor.StateCh <- serverState
+	}
+	if config.DevelopLogLevel >= 1 {
+		place := logger.Place()
+		logger.Debug(place, serverState.Name+" is booting up")
+	}
+
+	// 起動処理を発行，完了後の返却値受け取り
+	power.Info = config.Cluster.VirtualMachines[serverState.Name].Bootup(config.Sleep)
+	serverState.Info = power.Info
+	if monitor.PowerCh != nil {
+		monitor.PowerCh <- power
+	}
+	if monitor.StateCh != nil {
+		monitor.StateCh <- serverState
+	}
+	if config.DevelopLogLevel >= 1 {
+		place := logger.Place()
+		logger.Debug(place, serverState.Name+" is boot up")
+	}
+	mutex.Write(&totalWeight, &totalWeightMutex, totalWeight+serverState.Weight)
+}
+
 // shutDownVMs は引数に 設定値用構造体 config, 停止したいサーバの重み weight, 判断基準にした負荷量 load をとります．
-func shutDownVMs(config *models.Config, weight int, load string) {
+/*func shutDownVMs(config *models.Config, weight int, load string) {
 	for _, serverState := range monitor.GetServerStates() {
 		// 稼働中のサーバ以外は無視
 		if serverState.Info != "booted up" {
@@ -161,9 +299,80 @@ func shutDownVMs(config *models.Config, weight int, load string) {
 		}
 	}
 }
+*/
+
+func shutDownVMs(config *models.Config, weight int, load string) {
+	for _, serverState := range monitor.GetServerStates() {
+		// 稼働中のサーバ以外は無視
+		if serverState.Info != "booted up" {
+			continue
+		}
+		// オリジンサーバは無視
+		if config.ContainMachineName(config.OriginMachineNames, serverState.Name) {
+			continue
+		}
+		// 常に稼働するサーバは無視
+		if config.ContainMachineName(config.AlwaysRunningMachines, serverState.Name) {
+			continue
+		}
+
+		// 規定時間経過したサーバがあれば停止処理を発行
+		if serverState.Info == "waiting" && serverState.WaitTime <= time.Now() {
+			go shutDownVM(config, serverState, load)
+			mutex.Write(&totalWeight, &totalWeightMutex, totalWeight-serverState.Weight)
+			mutex.Write(&futureTotalWeight, &futureTotalWeightMutex, futureTotalWeight-serverState.Weight)
+			if config.DevelopLogLevel >= 1 {
+				place := logger.Place()
+				logger.Debug(place, serverState.Name+" going to shutdown")
+			}
+			return
+		} else if serverState.Info == "waiting" {
+			continue
+		}
+
+		if serverState.Weight <= weight {
+			sS := serverState
+		}
+	}
+	// サーバの重さが必要な重み以下なら停止処理を発行
+	sS.WaitTime = time.Now().Add(30 * time.Second)
+	sS.Info = "RMWait"
+	go waitVM(config, sS, load)
+	if config.DevelopLogLevel >= 1 {
+		place := logger.Place()
+		logger.Debug(place, serverState.Name+" going to wait")
+	}
+	return
+}
 
 //shutDownVM は引数に 設定値用構造体 config, 停止するサーバの情報 serverState, 判断基準にした負荷量 load をとります．
 func shutDownVM(config *models.Config, serverState monitor.ServerState, load string) {
+	var power monitor.PowerStruct
+	power.Name = serverState.Name
+	power.Info = "shutting down"
+	power.Load = load
+	if monitor.PowerCh != nil {
+		monitor.PowerCh <- power
+	}
+
+	serverState.Info = "shutting down"
+	if monitor.StateCh != nil {
+		monitor.StateCh <- serverState
+	}
+
+	// 停止処理を発行，完了後の返却値受け取り
+	power.Info = config.Cluster.VirtualMachines[serverState.Name].Shutdown(config.Sleep)
+	if monitor.PowerCh != nil {
+		monitor.PowerCh <- power
+	}
+
+	serverState.Info = power.Info
+	if monitor.StateCh != nil {
+		monitor.StateCh <- serverState
+	}
+}
+
+func waitVM(config *models.Config, serverState monitor.ServerState, load string) {
 	var power monitor.PowerStruct
 	power.Name = serverState.Name
 	power.Info = "shutting down"
